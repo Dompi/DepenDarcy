2a1a4492382d8826bd4062da01bd0ab02a3f8f0a
Balazs Blaschek <balazs.blaschek@betssongroup.com>
2019-07-05 16:51:39 +0200

    add Project

---
+++ DepenDarcy.Core/Entities/Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DepenDarcy.Core.Entities
+{
+    class Project
+    {
+        public List<string> Dependencies { get; set; }
+        public List<string> Dependents { get; set; }
+
+        public Project()
+        {
+            this.Dependencies = new List<string>();
+            this.Dependents = new List<string>();
+                 
+        }
+
+    }
+}
